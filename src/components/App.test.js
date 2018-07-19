4db8c706444c859a65189584d7f7733a392498e7
theperfectfuel <robert@theperfectfuel.com>
2018-07-19 15:04:51 -0700

    updated paths in test file

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import './index.css';
-import App from './components/App';
-import rootReducer from './reducers';
+import '../index.css';
+import App from './App';
+import rootReducer from '../reducers';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(rootReducer, {}, applyMiddleware(thunk));
